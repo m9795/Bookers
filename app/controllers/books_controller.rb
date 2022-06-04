127b4b0e669e5fb87e5e15a1db286acde9db91be
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-04 17:19:26 +0900

    [Remove] 3-12 コントローラnew,indexアクション修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
   
   def index
+    @book = Book.new
     @books = Book.all
   end
 
