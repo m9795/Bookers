4e35a31b00b0c265eba59c072074a741b6af57c3
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-04 13:33:45 +0900

    [Add] 3-12 ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root:to => 'homes#top'
+  resources :books
 end
